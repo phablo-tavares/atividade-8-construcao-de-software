2d35d51eb22e53a9a2af955ee8ddb0e2c909ce14
phablo <phablo.tavares1@gmail.com>
2024-04-26 15:10:31 -0300

    primeiro commit

---
+++ questao1.java
@@ -0,0 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class questao1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1;
+        int n2;
+        int result;
+        try{
+            n1 = sc.nextInt();
+            n2 = sc.nextInt();
+            result = (int)n1/n2;
+            System.out.println("Resultado da divisão: "+ result);
+        }
+        catch( ArithmeticException e ){
+            System.out.println("Não pode dividir por 0");
+        }
+        catch( InputMismatchException e){
+            System.out.println("Valor não numérico informado");
+        }
+        sc.close();
+    }
+}
